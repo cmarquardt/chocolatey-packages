ef1aa73aa6be04a71d229f0b7a169bac4f66c157
Christian Marquardt <christian@marquardt.sc>
2017-05-01 17:18:44 +0200

    Added system-wide environment variable for Docker-Toolbox.

--- automatic/docker-toolbox-eum/tools/chocolateyInstall.ps1
+++ automatic/docker-toolbox-eum/tools/chocolateyInstall.ps1
@@ -24,5 +24,17 @@ $packageArgs = @{
 Install-ChocolateyPackage @packageArgs
 
 # Update system's PATH variable
+#
+# Note: Docker-Toolbox only updates the PATH variable for the installing user (e.g., localadmin),
+#       so we updated it globally here, after having disabled the installers handling of the
+#       PATH environment variables above.
 
 Install-ChocolateyPath -PathToInstall "$($env:ProgramFiles)\Docker Toolbox" -PathType 'Machine'
+
+# Add a DOCKER_TOOLBOX_INSTALL_PATH to the system-wide environment (for ConEmu)
+#
+# Note: Docker-Toolbox only sets this variable for the installing user (e.g., localadmin),
+#       so we set it globally here, and remove the user specific version.
+
+Install-ChocolateyEnvironmentVariable -VariableName "DOCKER_TOOLBOX_INSTALL_PATH" -VariableValue "$($env:ProgramFiles)\Docker Toolbox" -VariableType 'Machine'
+Install-ChocolateyEnvironmentVariable -VariableName "DOCKER_TOOLBOX_INSTALL_PATH" -VariableValue $null
