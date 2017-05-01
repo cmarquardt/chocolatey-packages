f84fee3596c7d2a4aef60bee0a8964c1669576f8
Christian Marquardt <christian@marquardt.sc>
2017-05-01 16:38:11 +0200

    Fixed default installation components and tasks for docker-toolbox-eum.

--- automatic/docker-toolbox-eum/tools/chocolateyInstall.ps1
+++ automatic/docker-toolbox-eum/tools/chocolateyInstall.ps1
@@ -17,7 +17,7 @@ $packageArgs = @{
   checksum      = '9157ED0DB9CAC984FBD5F308B7CF92F27C449185EFA42E64EAAF3C7A747F5144'
   checksumType  = 'sha256'
 
-  silentArgs    = "/TASKS=`"desktopicon`" /silent /norestart /log=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
+  silentArgs    = "/COMPONENTS=`"Docker,DockerMachine,DockerCompose,Kitematic`" /TASKS=`"desktopicon`" /silent /norestart /log=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
